d1dfd1ceb980cb1c108e3d320d18c89ffc6c9544
rahul <rahulk123@gmail.com>
2023-05-08 00:28:35 +0530

    first commit

---
+++ server/mongodb/connect.js
@@ -0,0 +1,11 @@
+import mongoose, { connect } from "mongoose";
+
+const connectDB = (url)=>{
+    mongoose.set('strictQuery',true);
+
+    mongoose.connect(url)
+    .then(()=> console.log('MongoDB connect'))
+    .catch((err)=> console.log(err));
+}
+
+export default connectDB;
